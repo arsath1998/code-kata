43efacb3f48346606cc9ff33c65ecd09d07d05bb
arsath1998 <47441409+arsath1998@users.noreply.github.com>
2019-02-19 08:46:39 +0530

    Create armstrongbetween.py

---
+++ armstrongbetween.py
@@ -0,0 +1,12 @@
+a=int(a)
+b=int(b)
+for i in range(a,b):
+   d = len(str(i))
+   sum = 0
+   c = i
+   while c > 0:
+       e = c % 10
+       sum += e ** d
+       c //= 10
+   if i == sum:
+       print(i),
